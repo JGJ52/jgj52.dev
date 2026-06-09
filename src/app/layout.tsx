8f47d3cae9afe727d2b69bee78e075dfdbbbf280
jgj52 <email@jgj52.hu>
2026-06-09 22:18:10 +0200

    remove shadow and fix that it was scrollable

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -36,7 +36,7 @@ export default function RootLayout({
       lang="en"
       className={`${poppins.variable} ${fredoka.variable} ${rubik.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="flex flex-col">{children}</body>
     </html>
   );
 }
