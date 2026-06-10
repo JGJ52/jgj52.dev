37618176358e8d04656dc765bd7d889e110cd526
jgj52 <email@jgj52.hu>
2026-06-10 23:19:50 +0200

    continue making it cool

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,13 +5,15 @@ export default function Home() {
     type project = {
         name: string,
         url: string,
+        description: string,
     }
 
     const projects: project[] = [
         {
             name: "Capey",
             url: "https://capey.app",
-        }
+            description: "Have a custom cape which others who use this mod can see too",
+        },
     ]
 
     return (
@@ -19,10 +21,12 @@ export default function Home() {
             <Navbar />
             <div className="flex flex-col items-center justify-between pt-12">
                 <p className={"text-4xl rubik"}>My projects</p>
-                <div className="flex flex-wrap mt-12">
+                <div className="flex flex-wrap mt-12 gap m-4">
                     {projects.map((project) => (
                         <a key={project.name} className={"project"} href={project.url}>
                             <p className={"text-2xl fredoka"}>{project.name}</p>
+                            <div className={"rounded-lg border-2 border-[#1a1b1c] mt-2 mb-2"}></div>
+                            <p className={"opacity-60"}>{project.description}</p>
                         </a>
                     ))}
                 </div>
