2ed7f13441073bb745bd4e973454738290e76443
jgj52 <email@jgj52.hu>
2026-06-10 23:46:22 +0200

    add width limit and more projects

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,6 +14,21 @@ export default function Home() {
             url: "https://capey.app",
             description: "Have a custom cape which others who use this mod can see too",
         },
+        {
+            name: "RankedTagger",
+            url: "https://modrinth.com/mod/rankedtagger",
+            description: "A mod that lets you see your tier on Ranked.hu in-game",
+        },
+        {
+            name: "BlockStop",
+            url: "https://modrinth.com/mod/blockstop",
+            description: "Stops you from breaking blocks you don't want to break",
+        },
+        {
+            name: "ScreenAPI",
+            url: "https://modrinth.com/mod/screenapi",
+            description: "Makes screen making easier for mod developers",
+        },
     ]
 
     return (
@@ -21,7 +36,7 @@ export default function Home() {
             <Navbar />
             <div className="flex flex-col items-center justify-between pt-12">
                 <p className={"text-4xl rubik"}>My projects</p>
-                <div className="flex flex-wrap mt-12 gap m-4">
+                <div className="flex flex-wrap mt-12 gap m-4 gap-4">
                     {projects.map((project) => (
                         <a key={project.name} className={"project"} href={project.url}>
                             <p className={"text-2xl fredoka"}>{project.name}</p>
